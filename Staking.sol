f87fe7d795f99ceea7ca210b4db9478a89013c82
G_E_N <g_bortnikov@list.ru>
2021-12-06 17:50:56 +0700

    audit

--- Staking.sol
+++ Staking.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "hardhat/console.sol";
 
 contract Staking is AccessControl, ReentrancyGuard {
     using SafeERC20 for IERC20;
